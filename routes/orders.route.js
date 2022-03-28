f830979ea4f79b91c5fe735a82fa61d2bbefbeb9
gataev <email>
2022-03-28 12:29:04 +0300

    Изменение в названия в роуте и контроллере

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -4,8 +4,8 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = Router();
 
-router.get('/purchaseds', ordersController.getAllOrders);
-router.get('/purchased/user', authMiddleware, ordersController.getOrderById);
-router.patch('/purchased/user', authMiddleware, ordersController.addItemToOrder);
+router.get('/orders', ordersController.getAllOrders);
+router.get('/orders/user', authMiddleware, ordersController.getOrderById);
+router.patch('/orders/user', authMiddleware, ordersController.addItemsToOrder);
 
 module.exports = router;
